c519ad24d4ed6df4870aae878fc7137c5c208e2b
Yousef-Elkhawanki <dev.elkhawanki@gmail.com>
2024-04-04 05:45:40 +0200

    implament video ui

---
+++ src/components/video/Video.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Video = () => {
+  return (
+    <section className="video">
+      <div className="play">
+        <span>play</span>
+      </div>
+    </section>
+  );
+};
+
+export default Video;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../../components/banner/Banner";
 import MostSelling from "../../components/mostSelling/MostSelling";
+import Video from "../../components/video/Video";
 
 const Home = () => {
   return (
     <main>
       <Banner />
       <MostSelling />
+      <Video />
     </main>
   );
 };
